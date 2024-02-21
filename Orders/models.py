d31d6eb2784ad723d6f806eba93727388b8b899d
Ivan Zabaluev <ivaiika@yandex.ru>
2024-02-21 22:13:15 +0300

    send mail

--- Orders/models.py
+++ Orders/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db import models
 
 from user.models import User
@@ -9,9 +11,9 @@ from user.models import User
 class Order(models.Model):
 
     ORDER_STATUS = (
-        (0, 'Досталвено'),
-        (1, 'Отменен'),
-        (2, 'Неоплачено'),
+        ('Досталвено', 'Досталвено'),
+        ('Отменен', 'Отменен'),
+        ('Неоплачено', 'Неоплачено'),
     )
 
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, blank=True, verbose_name='Покупатель')
@@ -20,6 +22,17 @@ class Order(models.Model):
     status = models.CharField(choices=ORDER_STATUS, max_length=10, verbose_name='Статус заказа')
     order_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Сумма заказа')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.status == Order.objects.get(id=self.id).status:
+            send_mail(f'Статус заказа {self.id} изменился',
+                      f'Статус вашего заказа изменился на {self.status}',
+                      settings.EMAIL_HOST_USER,
+                      [f'{self.user.email}'],
+                      fail_silently=False
+                      )
+        super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+
 
 
 
