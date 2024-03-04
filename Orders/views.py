b54350089d19f1b272a882c84ceaf6e128ac9202
Ivan Zabaluev <ivaiika@yandex.ru>
2024-03-04 16:32:48 +0300

    formatting

--- Orders/views.py
+++ Orders/views.py
@@ -1,3 +1,4 @@
+from rest_framework.serializers import Serializer
 import simplejson as json
 
 from drf_spectacular.utils import extend_schema
@@ -15,30 +16,41 @@ from goods.serializers import BasketSerializer
 
 
 @extend_schema(tags=["Order"])
-class OrderListViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class OrderListViewSet(
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated]
-    ordering_fields = ['status', 'order_price']
-    search_fields = ['status']
+    ordering_fields = ["status", "order_price"]
+    search_fields = ["status"]
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
 
-@extend_schema(tags=['UserInfo'])
+@extend_schema(tags=["UserInfo"])
 class UserInfoViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserInfoSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: Serializer):
         user = self.request.user
         user_info = serializer.save(user=user)
         basket = Basket.objects.filter(user=user)
-        basket_ser = BasketSerializer(basket, many=True, context={'request': self.request})
+        basket_ser = BasketSerializer(
+            basket, many=True, context={"request": self.request}
+        )
         order_price = basket.total_sum()
         status = 2
 
-        basket_json = json.dumps(basket_ser.data, use_decimal=True, ensure_ascii=False, encoding="utf-8")
-        Order.objects.create(user=user, user_info=user_info, basket=basket_json, status=status, order_price=order_price)
+        basket_json = json.dumps(
+            basket_ser.data, use_decimal=True, ensure_ascii=False, encoding="utf-8"
+        )
+        Order.objects.create(
+            user=user,
+            user_info=user_info,
+            basket=basket_json,
+            status=status,
+            order_price=order_price,
+        )
         basket.delete()
-
